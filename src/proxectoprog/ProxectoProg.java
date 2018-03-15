fb907847999c558fbe531f17598538a9adcd6239
Luis <luisfrb56@gmail.com>
2018-03-15 13:05:33 +0100

    Interfaz

--- src/proxectoprog/ProxectoProg.java
+++ src/proxectoprog/ProxectoProg.java
@@ -21,18 +21,16 @@ public class ProxectoProg {
         System.out.println(t1.getNumbarcos());
         System.out.println(t1.getTamañotab());
         t1.elixbarcos();
-<<<<<<< HEAD
         t1.crearTab();
         t1.montarTab();
         t1.visualizartab();
-=======
 //        Puntuacion p1=new Puntuacion();
 //        Xogadores x1=new Xogadores();
 //        PuntuacionFich pf1=new PuntuacionFich();
 //        p1.addScore(x1);
 //        pf1.guardar(p1);
->>>>>>> 62acd517b1844c2d19dede4e9bbbdef7b4dbbd62
-        
+
+       
     }
     
 }
