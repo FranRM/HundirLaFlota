97f99b55e36c9d0e56cb8f5fde8ac3c8627dc68d
unknown <chiscazo.paco@gmail.com>
2018-03-20 19:34:11 +0100

    Avances puntuacion.

--- src/com/fran/xogadores/Puntuacion.java
+++ src/com/fran/xogadores/Puntuacion.java
@@ -1,8 +1,8 @@
 
 package com.fran.xogadores;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -10,11 +10,22 @@ import proxectoprog.Tableiro;
 
 public class Puntuacion {
     File archivo=null;
-    FileWriter fichero = null;
     PrintWriter pw = null;
-    FileReader fr = null;
-    BufferedReader br = null;
     private String username;
+    private int puntuacion=0;
+    ArrayList <Puntuacion>score=new ArrayList();
+    Comparator c1=new Comparator<Puntuacion>() {
+        @Override
+        public int compare(Puntuacion p, Puntuacion p2) {
+            if(p.puntuacion>p2.puntuacion){//ordenamos de mayor a menor
+         return -1;
+      }else if(p.puntuacion<p2.puntuacion){
+         return 1;
+      }else{
+         return 0;
+      }
+   }
+    };
 
     public String getUsername() {
         return username;
@@ -23,11 +34,12 @@ public class Puntuacion {
     public void setUsername(String username) {
         this.username = username;
     }
-    private int puntuacion=0;
-
-    public Puntuacion() {
+    
+    public Puntuacion(){}
+    public Puntuacion( String nome, int puntos) {
+        username=nome;
+        puntuacion=puntos;
     }
-
     public int getPuntuacion() {
         return puntuacion;
     }
@@ -35,56 +47,47 @@ public class Puntuacion {
     public void setPuntuacion(int puntuacion) {
         this.puntuacion = puntuacion;
     }
-public void sumPuntuacion(){
-   if(Tableiro.acertoH>0){
-       puntuacion=Tableiro.acertoH*10;
-   }
-}
+    public void sumPuntuacion(){
+           puntuacion=Tableiro.acertoH*10;
+           score.add(new Puntuacion(username,puntuacion));
+    }
 
 public void guardarFichero(){
-     try
-        {
-            fichero = new FileWriter("puntuacionFich.txt",true);
-            pw = new PrintWriter(fichero);
+     try{
+            archivo = new File("puntuacionFich.txt");
+            pw = new PrintWriter(archivo);
 
-            for (int i = 0; i < 1; i++)
-                pw.println(username+": "+puntuacion+" Puntos");
+            for (Puntuacion contador:score){
+                pw.println(contador.username+","+contador.puntuacion);
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-           try {
-           if (null != fichero)
-              fichero.close();
-           } catch (IOException e2) {
-              e2.printStackTrace();
-           }
-        }
+        }catch (FileNotFoundException fnfe1){
+                pw.close();
+        } 
     }
+
 public void leerFicheiro(){
+        Scanner sc=null;
     try{
-        
-        Scanner sc;
-    archivo = new File ("puntuacionFich.txt");
-         sc= new Scanner(archivo);   
-         
+        archivo = new File ("puntuacionFich.txt");
+         sc= new Scanner(archivo).useDelimiter(",\\s*");   
          while(sc.hasNext()){
-           JOptionPane.showMessageDialog(null,sc.nextLine());
+           score.add(new Puntuacion(sc.next(),Integer.parseInt(sc.next())));
          }
-    
-      }catch(Exception e){
-         e.printStackTrace();
+      }catch(FileNotFoundException fnfe1){
+         System.out.println("Error:"+fnfe1.getMessage());
       }finally{
-        
-         try{                    
-            if( null != fr ){   
-               fr.close();     
-            }                  
-         }catch (IOException e2){ 
-            e2.printStackTrace();
-         }
+       sc.close();
       }
    }
-
+public String vertablascore(){
+    String salida="*****Score*****";
+    for(Puntuacion c:score){
+        salida="\n"+salida+c.username+"-->"+c.puntuacion;
+    }
+    return salida;
 }
-
+    public void ordear(){
+        score.sort(c1);
+    }
+}
