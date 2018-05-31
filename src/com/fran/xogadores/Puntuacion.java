2f5da8e87661d18e5c581e004d103cc05a8d0d97
Luis <luisfrb56@gmail.com>
2018-05-31 14:42:50 +0200

    Puntuacion en proceso

--- src/com/fran/xogadores/Puntuacion.java
+++ src/com/fran/xogadores/Puntuacion.java
@@ -6,47 +6,54 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import proxectoprog.Tableiro;
 
 public class Puntuacion {
-    private Connection conector;
+    
+    Connection connect;
+    ResultSet rs ;
+    Statement s;
     private String ruta="Puntuacion.db";
     ArrayList <Xogador>lista=new ArrayList();
+ 
+    
     public Puntuacion(){}
     
     
     
     public void conectar(){
         try{
-        conector=DriverManager.getConnection("jdbc:sqlite:"+ruta);
-        if(conector!=null){
+        connect=DriverManager.getConnection("jdbc:sqlite:"+ruta);
+        if(connect!=null){
             System.out.println("Base conectada.");
         }
         }catch(SQLException sqe1){
             System.out.println("Erro:"+sqe1.getMessage());
         }
     }
-    public void consultar(){
-        try {
-            ResultSet resul;
-            PreparedStatement con=conector.prepareStatement("select * from xogador;");
-            resul=con.executeQuery();
-            while(resul.next()){
-            lista.add(new Xogador(resul.getString("nome"),resul.getInt("puntos")));
-            }
+    public void insertar(){
+    try {
+            PreparedStatement st = connect.prepareStatement("insert into xogador (nome, puntos) values (?,?)");
+            st.setString(1, this.getUsername());
+            st.setInt(2, this.getPuntuacion());
+
+            st.execute();
+
         } catch (SQLException ex) {
-            Logger.getLogger(Puntuacion.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex.getMessage());
         }
-    }
+}
     public void pechar(){
         try {
-            conector.close();
+            connect.close();
         } catch (SQLException ex) {
             Logger.getLogger(Puntuacion.class.getName()).log(Level.SEVERE, null, ex);
         }
